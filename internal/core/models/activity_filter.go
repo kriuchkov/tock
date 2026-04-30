066be0c13cd7b6a1a5bf77a1b357a80d0030926f
SMBH <smbhop@gmail.com>
2026-04-30 11:17:06 +0330

    :fire: Update(refactor): Flatten activity filter logic

--- internal/core/models/activity_filter.go
+++ internal/core/models/activity_filter.go
@@ -20,49 +20,48 @@ type ActivityFilterOptions struct {
 }
 
 func BuildActivityFilter(opts ActivityFilterOptions) (ActivityFilter, error) {
-	now := opts.Now
-	if now.IsZero() {
-		now = time.Now()
-	}
+    now := opts.Now
+    if now.IsZero() {
+        now = time.Now()
+    }
 
-	filter := ActivityFilter{}
+    filter := ActivityFilter{}
 
-	if opts.FromDate != nil || opts.ToDate != nil {
-		if opts.FromDate != nil {
-			filter.FromDate = opts.FromDate
-		}
-		if opts.ToDate != nil {
-			filter.ToDate = opts.ToDate
-		}
-	} else {
-		switch {
-		case opts.Today:
-			start, end := timeutil.LocalDayBounds(now)
-			filter.FromDate = &start
-			filter.ToDate = &end
-		case opts.Yesterday:
-			todayStart, _ := timeutil.LocalDayBounds(now)
-			start := todayStart.AddDate(0, 0, -1)
-			end := todayStart
-			filter.FromDate = &start
-			filter.ToDate = &end
-		case opts.Date != "":
-			parsedDate, err := time.ParseInLocation("2006-01-02", opts.Date, time.Local)
-			if err != nil {
-				return ActivityFilter{}, errors.Wrap(err, "invalid date format (use YYYY-MM-DD)")
-			}
-			start, end := timeutil.LocalDayBounds(parsedDate)
-			filter.FromDate = &start
-			filter.ToDate = &end
-		}
-	}
+    switch {
+    case opts.FromDate != nil || opts.ToDate != nil:
+        if opts.FromDate != nil {
+            filter.FromDate = opts.FromDate
+        }
+        if opts.ToDate != nil {
+            filter.ToDate = opts.ToDate
+        }
+    case opts.Today:
+        start, end := timeutil.LocalDayBounds(now)
+        filter.FromDate = &start
+        filter.ToDate = &end
+    case opts.Yesterday:
+        todayStart, _ := timeutil.LocalDayBounds(now)
+        start := todayStart.AddDate(0, 0, -1)
+        end := todayStart
+        filter.FromDate = &start
+        filter.ToDate = &end
+    case opts.Date != "":
+        parsedDate, err := time.ParseInLocation("2006-01-02", opts.Date, time.Local)
+        if err != nil {
+            return ActivityFilter{}, errors.Wrap(err, "invalid date format (use YYYY-MM-DD)")
+        }
+        start, end := timeutil.LocalDayBounds(parsedDate)
+        filter.FromDate = &start
+        filter.ToDate = &end
+    }
 
-	if opts.Project != "" {
-		filter.Project = &opts.Project
-	}
-	if opts.Description != "" {
-		filter.Description = &opts.Description
-	}
+    if opts.Project != "" {
+        filter.Project = &opts.Project
+    }
+    if opts.Description != "" {
+        filter.Description = &opts.Description
+    }
 
-	return filter, nil
+    return filter, nil
 }
+
